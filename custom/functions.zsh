1b6a2c8e9a896743ea1dfa14fd802c8a8af6dbaf
Franklin Henderson <fhwrdh@fhwrdh.net>
2014-09-10 17:38:53 -0700

    changed git prompt to include repo name

--- custom/functions.zsh
+++ custom/functions.zsh
@@ -10,6 +10,15 @@ function git_show_change() {
     git show $(git blame ${FILE} -L ${LINE},${LINE} | awk '{print $1}')
 }
 
+function git_prompt_info() {
+  if [[ "$(command git config --get oh-my-zsh.hide-status 2>/dev/null)" != "1" ]]; then
+    repo=$(command basename `git rev-parse --show-toplevel 2> /dev/null` 2> /dev/null) || return 0
+    ref=$(command git symbolic-ref HEAD 2> /dev/null) || \
+    ref=$(command git rev-parse --short HEAD 2> /dev/null) || return 0
+    echo "$ZSH_THEME_GIT_PROMPT_PREFIX${repo}:${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+  fi
+}
+
 function be() {
     sudo -u $@ -H bash login
 }
